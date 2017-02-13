16b8a2cdbcc27123f06936774c5e59eefa2e9e40
cristian <cristian@192.168.90.139>
2017-02-13 10:29:05 -0300

    Controller

--- config/controller.config.php
+++ config/controller.config.php
@@ -6,7 +6,9 @@ return [
     'controllers' => [
         'factories' => [
             Controller\LoginController::class => Factory\Controller\LoginControllerFactory::class,
-            Controller\RegisterController::class => Factory\Controller\RegisterControllerFactory::class
+            Controller\RegisterController::class => Factory\Controller\RegisterControllerFactory::class,
+            Controller\AdminUserController::class => Factory\Controller\AdminUserControllerFactory::class
+       
         ]
     ]
 ];
