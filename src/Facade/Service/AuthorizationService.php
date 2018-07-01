26fc94ce8c195733b880dd588fc0d6d4d8e40ccc
crist <cristian.cdi@gmail.com>
2018-07-01 10:37:16 -0300

    Facade AuthorizationService (isGranted)

---
+++ src/Facade/Service/AuthorizationService.php
@@ -0,0 +1,9 @@
+<?php
+
+class AuthorizationService extends \ZfMetal\Commons\Facade\Accessor
+{
+    public static function getServiceName()
+    {
+        return 'ZfcRbac\Service\AuthorizationService';
+    }
+}
