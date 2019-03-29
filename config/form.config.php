d9d2b2b41ccd9d6f3737b952614c14f121f42401
cristian.cdi@gmail.com <Neverwinter2>
2019-03-29 19:51:18 -0300

    Se registra form Register en factory

--- config/form.config.php
+++ config/form.config.php
@@ -4,7 +4,7 @@ return [
     'form_elements' => [
         'factories' => [
             \ZfMetal\Security\Form\User::class => \Zend\ServiceManager\Factory\InvokableFactory::class,
-
+            \ZfMetal\Security\Form\Register::class => \Zend\ServiceManager\Factory\InvokableFactory::class,
         ],
 
     ],
